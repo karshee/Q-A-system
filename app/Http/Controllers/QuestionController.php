759e1747fcf9a0ba5a9147397b82aff7edf2d0ce
kasum hussain <kasumhussain96@gmail.com>
2018-10-16 14:21:36 +0100

    'lol'

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    function index()
+    {
+        return 'This is the list of questions';
+    }
+}
